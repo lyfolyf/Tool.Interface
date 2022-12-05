df61a1487b263041af7e8ee606a64ab6d8ec44c4
Jake li <lyfolyf@163.com>
2022-12-05 10:13:47 +0800

    添加项目文件。

---
+++ ICreat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lead.Tool.Interface
+{
+    public interface ICreat
+    {
+        string Name { get; }
+        Image Icon { get; }
+        ITool GetInstance(string Name, string ConfigPath);
+    }
+}
---
+++ ITool.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lead.Tool.Interface
+{
+    public enum IToolState
+    {
+        ToolMin = 0,
+        ToolInit,
+        ToolRunning,
+        ToolTerminate
+    }
+    public interface ITool
+    {
+        IToolState State { get; }
+        Control ConfigUI { get; }
+        Control DebugUI { get; }
+        void Init();
+        void Start();
+        void Terminate();
+    }
+}
